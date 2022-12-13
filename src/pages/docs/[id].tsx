1633aea37dbe1f06bc212faa2afa54ef7c8ed608
Awin Abi <awin@awinawin.awin>
2022-12-13 17:01:29 +0530

    remove unwanted Layout component import

--- src/pages/docs/[id].tsx
+++ src/pages/docs/[id].tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../../../components/Layout";
+
 import { getDocPages, getDocPage } from "../../utils/api";
 
 interface DocPage {
--- src/pages/docs/alt.tsx
+++ src/pages/docs/alt.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Layout from "../../../components/Layout";
 import { getDocPages, getDocPage } from "../../utils/api";
 
 interface DocPage {
